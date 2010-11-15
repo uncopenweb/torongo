419733a17992d16cd730eccfe276e483b9ce82fd
Gary Bishop <gb@cs.unc.edu>
2010-11-15 10:24:30 -0500

    continued work on media upload.

--- server/upload.py
+++ server/upload.py
@@ -23,30 +23,38 @@ import subprocess
 import json
 import pymongo
 import mongo_util
+import mad
 
 MediaDB = 'Media'
 MediaURL = '/Media/'
 MediaRoot = '/var/Media/'
+
+class Bag(object):
+    pass
 
 class UploadHandler(access.BaseHandler):
     @tornado.web.asynchronous
     def post(self):
         '''Create a new item and return the single item not an array'''
         args = self.request.arguments
+        info = Bag()
         try:
-            tags = [ tag for tag in args['tags'][0].strip().split()
-                     if re.match(r'^\w+$', tag) ]
-            fname = args['file.name'][0].strip()
-            ftype = args['file.content_type'][0].strip()
-            fpath = args['file.path'][0].strip()
+            info.tags = [ tag for tag in args['tags'][0].strip().split()
+                          if re.match(r'^\w+$', tag) ]
+            info.fname = args['file.name'][0].strip()
+            info.ftype = args['file.content_type'][0].strip()
+            info.fpath = args['file.path'][0].strip()
             key = args['Authorization'][0]
+            info.title = args.get('title', [''])[0]
+            info.description = args.get('description', [''])[0]
+            info.creditURL = args.get('creditURL', [''])[0]
         except Exception, e:
             raise HTTPError(400, unicode(e))
 
-        if ftype.startswith('image/'):
+        if info.ftype.startswith('image/'):
             worker = self._imageWorker
             medium = 'Image'
-        elif ftype.startswith('audio/'):
+        elif info.ftype.startswith('audio/'):
             worker = self._audioWorker
             medium = 'Audio'
         else:
@@ -57,29 +65,27 @@ class UploadHandler(access.BaseHandler):
 
         collection = self.mongo_conn[MediaDB][medium]
 
-        self.run_async(self._callback, worker, collection, tags, fname, ftype, fpath)
+        self.run_async(self._callback, worker, collection, info)
 
-    def _imageWorker(self, collection, tags, fname, ftype, fpath):
+    def _imageWorker(self, collection, info):
         acceptable_extensions = set(['.jpg', '.gif', '.jpeg', '.bmp', '.png', '.tif', '.tiff',
                                     '.wbmp', '.jng', '.svg'])
-        print >>sys.stderr, '_imageWorker', tags, fname, ftype, fpath
-        root, ext = os.path.splitext(fname)
-        toRemove = [ fpath ]
+        root, ext = os.path.splitext(info.fname)
+        toRemove = [ info.fpath ]
         try:
             if ext not in acceptable_extensions:
                 raise TypeError('unknown extension')
             id = mongo_util.newId()
-            img = Image.open(file(fpath, 'rb'))
+            img = Image.open(file(info.fpath, 'rb'))
             w, h = img.size
             opath = os.path.join(MediaRoot, 'Image', id[-2:], id + ext)
             upath = os.path.join(MediaURL, 'Image', id + ext)
             dirname = os.path.dirname(opath)
             if not os.path.exists(dirname):
                 os.makedirs(dirname)
-            shutil.move(fpath, opath)
+            shutil.move(info.fpath, opath)
         except Exception, e:
-            traceback.print_exc()
-            return (False, e, None)
+            return (False, traceback.format_exc(), None)
         finally:
             for f in toRemove:
                 try:
@@ -90,24 +96,24 @@ class UploadHandler(access.BaseHandler):
         item = {}
         item['width'] = w
         item['height'] = h
-        item['originalName'] = fname
+        item['originalName'] = info.fname
         item['URL'] = upath
-        item['tags'] = tags
+        item['tags'] = info.tags
+        item['title'] = info.title
+        item['description'] = info.description
+        item['creditURL'] = info.creditURL
         user = self.get_current_user()
         item['uploadedBy'] = user['email']
         item['uploadedOn'] = datetime.now().isoformat()
         item['_id'] = id
 
-#        self.validateSchema(MediaDB, 'image', item)
-
         collection.insert(item, safe=True)
 
         return (True, item, 'Image')
 
-    def _audioWorker(self, collection, tags, fname, ftype, fpath):
-        print >>sys.stderr, '_audioWorker', tags, fname, ftype, fpath
-        root, ext = os.path.splitext(fname)
-        toRemove = [ fpath ]
+    def _audioWorker(self, collection, info):
+        root, ext = os.path.splitext(info.fname)
+        toRemove = [ info.fpath ]
         if not ext:
             return (False, 'bad extension', None)
 
@@ -119,52 +125,61 @@ class UploadHandler(access.BaseHandler):
             dirname = os.path.dirname(opath)
             if not os.path.exists(dirname):
                 os.makedirs(dirname)
-            if ftype == 'audio/midi':
+            if info.ftype == 'audio/midi':
                 # check the magic number to try to assure it is midi since timidity doesn't seem to error
                 ms = magic.open(magic.MAGIC_NONE)
                 ms.load()
-                if not re.search(r'\smidi\s', ms.file(fpath), re.I):
+                if not re.search(r'\smidi\s', ms.file(info.fpath), re.I):
                     raise TypeError('not midi')
-                tmp = fpath + '.wav'
+                tmp = info.fpath + '.wav'
                 toRemove.append(tmp)
-                r1 = subprocess.call([TIMIDITYPATH, '-Ow', '-idqq', fpath, '-o', tmp])
+                r1 = subprocess.call([TIMIDITYPATH, '-Ow', '-idqq', info.fpath, '-o', tmp])
                 if r1:
                     raise RuntimeError('midi decode failed')
             elif ext != '.wav':
-                tmp = fpath + '.wav'
-                r1 = subprocess.call([SOXPATH, '-t', ext, fpath, tmp])
+                tmp = info.fpath + '.wav'
+                r1 = subprocess.call([SOXPATH, '-t', ext, info.fpath, tmp])
                 toRemove.append(tmp)
                 if r1:
                     raise RuntimeError('wav decode failed')
             else:
-                tmp = fpath
+                tmp = info.fpath
             # at this point we should have a wav file in tmp
             r1 = subprocess.call([SOXPATH, '-t', '.wav', tmp, opath + '.mp3', 'rate', '22050', 'norm'])
             r2 = subprocess.call([SOXPATH, '-t', '.wav', tmp, opath + '.ogg', 'rate', '22050', 'norm'])
             if r1 or r2:
                 raise RuntimeError('encoding failed')
+                
+            # get the duration
+            mf = mad.MadFile(opath + '.mp3')
+            duration = mf.total_time() / 1000.0
+            
         except Exception, e:
             traceback.print_exc()
-            return (False, e, None)
+            return (False, traceback.format_exc(), None)
         finally:
             for f in toRemove:
                 try:
                     os.remove(f)
                 except:
                     pass
-
         item = {}
-        item['originalName'] = fname
+        item['originalName'] = info.fname
         item['URL'] = upath
-        item['tags'] = tags
+        item['tags'] = info.tags
+        item['title'] = info.title
+        item['description'] = info.description
+        item['creditURL'] = info.creditURL
         user = self.get_current_user()
         item['uploadedBy'] = user['email']
         item['uploadedOn'] = datetime.now().isoformat()
+        item['duration'] = duration
         item['_id'] = id
+
+        collection.insert(item, safe=True)
         return (True, item, 'Audio')
 
     def _callback(self, result, *args):
-        print >>sys.stderr, '_callback', result, args
         flag, value, medium = result
         if flag:
             # this path should get encoded only one place, fix this
@@ -173,7 +188,6 @@ class UploadHandler(access.BaseHandler):
             wrapper = '''<html><body><textarea>%s</textarea></body></html>'''
             s = wrapper % s
             self.set_header('Content-length', len(s))
-            # self.set_header('Content-type', 'application/json')
             self.write(s)
             self.finish()
         else:
